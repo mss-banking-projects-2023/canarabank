a130bf79cd4b82775aa17c8d01179c7749d649a5
devopstrainingblr <root@ip-172-31-40-148.ap-south-1.compute.internal>
2024-01-03 01:25:07 +0000

    Reverted .. Revert "added"
    This reverts commit 8876de6600bb0e8ae86df8a696797ccf806257c3.

--- DBUtils.java
+++ DBUtils.java
@@ -2,6 +2,3 @@ Hello this is the java file ..
 contains the DB relared script..
 
 hello again updating..
-
-hello added this line now..
-
--- StringUtils.java
+++ StringUtils.java
@@ -1,4 +1,2 @@
 Hello this file contains all the String related functions
 
-Hello added this line...
-
